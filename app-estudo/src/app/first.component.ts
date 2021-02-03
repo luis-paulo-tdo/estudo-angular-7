ebcc88d508ca2c315b9da725ec23f109129317e0
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-02-03 06:45:38 -0300

    Definindo um estilo para o primeiro Component criado. O Estilo de um Componente pode ser definido de três formas: (1) Dentro do próprio HTML do Componente através da tag <style>, independente se o HTML estiver dentro do atributo 'template' em um arquivo referênciado pelo 'templateUrl'; (2) Através da propriedade 'styles' pertencente ao Decorator @Component; (3) Através de um arquivo CSS separado que é referenciado pela propriedade 'styleUrls' do Decorator @Component.

--- app-estudo/src/app/first.component.ts
+++ app-estudo/src/app/first.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'study-first',
-    templateUrl: './first.component.html'
+    templateUrl: './first.component.html',
+    styleUrls: ['./first.component.css']
 })
 export class FirstComponent {
 
