b385e0a66668388dc9bcb5df01bbaf45d0751b02
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-02-03 07:09:49 -0300

    Alterando o seletor do primeiro Componente. Um Componente deve ter um único Seletor que será responsável por identificá-lo dentro de um template. Um Seletor, por sua vez, pode ser definido de três formas na propriedade 'selector' do Decorator @Component: (1) Na forma de tag, utilizando uma string comum ('study-first'); (2) Na forma de diretiva em uma tag HTML, envolvendo a string entre colchetes ('[study-first]'); (3) Na forma de classe CSS, prefixando a string com um caractere de ponto ('.study-first').

--- app-estudo/src/app/first.component.ts
+++ app-estudo/src/app/first.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'study-first',
+    selector: '.study-first',
     templateUrl: './first.component.html',
     styleUrls: ['./first.component.css']
 })
