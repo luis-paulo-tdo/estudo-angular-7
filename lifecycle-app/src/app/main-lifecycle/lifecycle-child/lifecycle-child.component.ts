01d4ea6f10fc709555d5e40ffbdac56cefbf143f
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-03-02 06:16:58 -0300

    Implementação de alguns dos Lifecyle Hooks para o Componente Child do Projeto. Lifecycle Hooks são callbacks que podem ser implementados para executar em uma ou mais etapas do ciclo de vida de um Componente Angular. Eles só serão executados apenas se forem implementados, embora nenhuma das implementações sejam obrigatórias. Após a execução do Construtor (que não é um Hook), os callbacks executados estão na seguinte ordem: OnChanges, OnInit, AfterContentInit, AfterViewInit.

--- lifecycle-app/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
+++ lifecycle-app/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
@@ -1,19 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'lifecycle-child',
   templateUrl: './lifecycle-child.component.html',
   styleUrls: ['./lifecycle-child.component.css']
 })
-export class LifecycleChildComponent implements OnInit {
+export class LifecycleChildComponent implements AfterContentInit, AfterViewInit, OnChanges, OnDestroy, OnInit {
 
   @Input() age: number;
   @Input() band: string;
   @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log('[Cons] Name:', this.name);
   }
 
+  ngOnInit() {
+    console.log('[Init] Name:', this.name);
+  }
+  
+  ngAfterContentInit() {
+    console.log('[Acin] Name:', this.name);
+  }
+
+  ngAfterViewInit() {
+    console.log('[Avin] Name:', this.name);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('[Chng] Name:', this.name);
+  }
+
+  ngOnDestroy() {
+    console.log('[Dstr] Name:', this.name);
+  }
 }
