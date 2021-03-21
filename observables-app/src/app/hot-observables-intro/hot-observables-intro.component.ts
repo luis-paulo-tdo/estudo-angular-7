89fcfd31cea781ebc47f4625ec3b64a232dbe3dc
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-03-21 11:58:21 -0300

    Criação de um Hot Observable a partir da classe Producer, que independente da quantidade de subscribes feitos nela, será a única produtora e emissora de dados. Encapsulando uma lista de listeners, um objeto instanciado a partir de um Producer permite a adição de novos listeners nesta lista. Na medida em que vai gerando novos valores, estes listeners são notificados, recebendo em seus respectivos callbacks o valor que foi produzido para que executem suas operações sobre ele. A classe também disponibiliza um método para interromper a produção e o envio de valores.

--- observables-app/src/app/hot-observables-intro/hot-observables-intro.component.ts
+++ observables-app/src/app/hot-observables-intro/hot-observables-intro.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { fromEvent, Observable } from 'rxjs';
+import { fromEvent, Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'observables-hot-observables-intro',
@@ -10,15 +10,70 @@ export class HotObservablesIntroComponent implements OnInit {
 
   numberOne: number = 0;
   numberTwo: number = 0;
-  stringOne: string = '';
-  stringTwo: string = '';
+  numberThree: number = 0;
+  numberFour: number = 0;
+  stringOne: string = 'Listener #1';
+  stringTwo: string = 'Listener #2';
+  stringThree: string = 'Listener #3';
+  stringFour: string = 'Listener #4';
   @ViewChild('button') button: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.createHotObservable();
+  }
+
+  private buttonHotObservable() {
     const buttonClicked: Observable<any> = fromEvent(this.button.nativeElement, 'click');
-    buttonClicked.subscribe(event => console.log(`[#1] Clicked Button`));
-    buttonClicked.subscribe(event => console.log(`[#2] Clicked Button`));
+    buttonClicked.subscribe(event => console.log(`[Subscribe #1] Clicked Button ${event}`));
+    buttonClicked.subscribe(event => console.log(`[Subscribe #2] Clicked Button ${event}`));
+  }
+
+  private createHotObservable() {
+    class Producer {
+      private id;
+      private value = 0;
+      private listeners = [];
+
+      addListener(listener) {
+        this.listeners.push(listener);
+        console.log('[Producer] You have', this.listeners.length, 'listener(s) now');
+      }
+
+      start() {
+        this.id = setInterval(() => {
+          this.value++;
+          console.log('[Producer] Value:', this.value);
+          for (let listener of this.listeners) {
+            listener(this.value);
+          }
+        }, 1000);
+      }
+
+      stop() {
+        clearInterval(this.id);
+      }
+    }
+
+    const producer: Producer = new Producer();
+    producer.start();
+
+    setTimeout(() => {
+      producer.addListener(value => this.numberOne = value);
+    }, 4000);
+
+    setTimeout(() => {
+      producer.addListener(value => this.numberTwo = value);
+    }, 8000);
+
+    const hotObservable = new Observable(
+      (observer: Observer<number>) => {
+        producer.addListener(value => observer.next(value));
+      }
+    );
+
+    hotObservable.subscribe(value => this.numberThree = value);
+    hotObservable.subscribe(value => this.numberFour = value);
   }
 }
