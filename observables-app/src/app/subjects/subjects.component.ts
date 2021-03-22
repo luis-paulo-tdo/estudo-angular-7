6dcd259c137d7c97454b7f7f6d2cce47aff21150
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-03-22 06:43:52 -0300

    Adição de botão para realizar conexão do Observable gerado pelo publish() com o Observer que irá produzir e emitir os valores. Foram também adicionados três tipos de Subjects além do convencional: Replay, Async e Behavior Subjects. Uma vez criados, foi feito o subscribe de cada um deles no Observable Publish.

--- observables-app/src/app/subjects/subjects.component.ts
+++ observables-app/src/app/subjects/subjects.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+import { DataModel } from '../data-model';
+import { RandomDataService } from '../random-data.service';
 
 @Component({
   selector: 'observables-subjects',
@@ -9,10 +11,30 @@ import { Subject } from 'rxjs';
 export class SubjectsComponent implements OnInit {
 
   value: number = 0;
+  private subject: Subject<DataModel>;
+  private replaySubject: ReplaySubject<DataModel>;
+  private asyncSubject: AsyncSubject<DataModel>;
+  private behaviorSubject: BehaviorSubject<DataModel>;
 
-  constructor() { }
+  constructor(private randomDataService: RandomDataService) { }
 
   ngOnInit() {
+    this.subject = new Subject<DataModel>();
+    this.replaySubject = new ReplaySubject<DataModel>();
+    this.asyncSubject = new AsyncSubject<DataModel>();
+    this.behaviorSubject = new BehaviorSubject<DataModel>({ timestamp: 0, value: 0 });
+
+    this.randomDataService.generator.subscribe(this.subject);
+    this.randomDataService.generator.subscribe(this.replaySubject);
+    this.randomDataService.generator.subscribe(this.asyncSubject);
+    this.randomDataService.generator.subscribe(this.behaviorSubject);
+  }
+
+  connect() {
+    this.randomDataService.generator.connect();
+  }
+
+  private useSubject() {
     const subject: Subject<number> = new Subject<number>();
     subject.subscribe(value => {
       this.value = value;
