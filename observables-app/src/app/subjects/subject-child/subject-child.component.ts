617b284c904826a1fcecc336faa8ed0b6446e7f4
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-03-22 07:22:40 -0300

    Criação de Sub Componente para demonstrar de forma discriminada a forma como cada Subject se comporta. Montagem do layout, configuração de CSS e implementação dos comportamentos básicos do Componente. Em sua versão final, o Componente apresenta um título que nomeia o Subject com o qual ele está atuando, um botão responsável por conectar/desconectar o Subject do subscribe e uma lista de logs que apresenta mensagens interativas e os valores númericos e timestamps que são emitidos pelo Subject durante o seu funcionamento.

---
+++ observables-app/src/app/subjects/subject-child/subject-child.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { DataModel } from 'src/app/data-model';
+
+@Component({
+  selector: 'observables-subject-child',
+  templateUrl: './subject-child.component.html',
+  styleUrls: ['./subject-child.component.css']
+})
+export class SubjectChildComponent implements OnInit {
+
+  @Input() name: string;
+  @Input() subject: Subject<DataModel>;
+
+  private connected: boolean = false;
+  private subscription: Subscription;
+  private logs: string[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  connect() {
+    this.logs.push('Connection estabilished!');
+    this.connected = true;
+    this.subscription = this.subject.subscribe(
+      data => this.log(data),
+      error => this.logs.push(`Error: ${error}`),
+      () => this.logs.push('Task finished!')
+    );
+  }
+
+  disconnect() {
+    this.logs.push('Subject disconnected.');
+    this.connected = false;
+  }
+
+  private log(data: DataModel) {
+    this.logs.push(`Timestamp: ${data.timestamp / 1000} | Data: ${data.value}`);
+  }
+}
