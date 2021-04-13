dbd1a3f3e9e59656b5bfaee7ccd6c67348f6d94d
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-13 06:16:35 -0300

    Demonstração do uso dos operadores take(quantity), first() e last() na obtenção dos valores que são produzidos por um Observable. O take(quantity) recebe um valor numérico que indica uma quantidade determinada de valores gerados pelos eventos next() que devem passar pelo pipe(), encerrando automaticamente o Observer que produtor uma vez que esta quantidade tenha sido produzida. O first() captura apenas o valor emitido pelo primeiro next(), encerrando o Observer logo em seguida. Por fim, o last() deixa todos os next() emitirem valores e captura apenas a última emissão, também encerrando o Observer após ela. Foi mostrada também uma forma de verificar se um Subscription foi encerrado ou não através do atributo closed.

--- rxjs-app/src/app/operators/operators.component.ts
+++ rxjs-app/src/app/operators/operators.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { from, fromEvent, interval } from 'rxjs';
-import { delay, filter, map, tap } from 'rxjs/operators';
+import { from, fromEvent, interval, Observable, Subscription } from 'rxjs';
+import { delay, filter, first, last, map, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'rxjs-operators',
@@ -72,5 +72,58 @@ export class OperatorsComponent implements OnInit {
         console.log('[Tap] Result:', interval);
         console.log('---');
       });
+  }
+
+  takeOperator() {
+    const observable = this.randomNumbersObservable(20);
+    const subscription: Subscription = this.testObservable('Take', observable, take(10));
+    this.checkSubscription(subscription);
+  }
+
+  firstOperator() {
+    const observable = this.randomNumbersObservable(20);
+    const subscription: Subscription = this.testObservable('First', observable, first());
+    this.checkSubscription(subscription);
+  }
+
+  lastOperator() {
+    const observable = this.randomNumbersObservable(20);
+    const subscription: Subscription = this.testObservable('Last', observable, last());
+    this.checkSubscription(subscription);
+  }
+
+  private randomNumbersObservable(quantity: number): Observable<number> {
+    return new Observable(observer => {
+      let index;
+      for (index = 0; index < quantity; ++index) {
+        setTimeout(() => observer.next(Math.floor(Math.random() * 100)), index * 100);
+      }
+      setTimeout(() => observer.complete(), index * 100);
+    });
+  }
+
+  private testObservable(name: string, observable: Observable<number>, callback: any): Subscription {
+    return observable
+      .pipe(
+        tap(value => console.warn(`[${name}] Testing => ${value}`)),
+        callback
+      )
+      .subscribe(
+        value => console.log(`[${name}] Value: ${value}`),
+        error => console.error(`[${name}] Error: ${error}`),
+        () => console.log(`[${name}] Completed!`)
+      );
+  }
+
+  private checkSubscription(subscription: Subscription) {
+    const interval = setInterval(
+      () => {
+        console.log('[Interval] Checking Subscription');
+        if (subscription.closed) {
+          console.log('[Interval] Subscription Closed!');
+          clearInterval(interval);
+        }
+      }, 200
+    );
   }
 }
