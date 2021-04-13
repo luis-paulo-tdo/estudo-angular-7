a077dcdf994ebf89bef92074d106de4d9c748b7d
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-13 05:31:14 -0300

    Demonstração do uso do operador tap() na execução de tarefas adicionais ao processo de tratamento de dados que é realizado pelos operadores map() e filter(). O tap() pode ser chamado em qualquer ponto da cadeia do pipe(), e geralmente é utilizado para realizar operações de log, alocações, eventos, manipulação do DOM, etc.

--- rxjs-app/src/app/operators/operators.component.ts
+++ rxjs-app/src/app/operators/operators.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { from, fromEvent, interval } from 'rxjs';
-import { delay, filter, map } from 'rxjs/operators';
+import { delay, filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'rxjs-operators',
@@ -19,18 +19,18 @@ export class OperatorsComponent implements OnInit {
     const observableFromEvent = fromEvent(document, 'click');
 
     observableFrom
-    .pipe(
-      map(value => value.toUpperCase()),
-      map(value => value.toLowerCase()),
-      delay(2000)
-    )
-    .subscribe(value => console.log(value));
+      .pipe(
+        map(value => value.toUpperCase()),
+        map(value => value.toLowerCase()),
+        delay(2000)
+      )
+      .subscribe(value => console.log(value));
 
     observableFromEvent
-    .pipe(
-      map((click: MouseEvent) => ({ x: click.screenX, y: click.screenY }))
-    )
-    .subscribe(value => console.log(value));
+      .pipe(
+        map((click: MouseEvent) => ({ x: click.screenX, y: click.screenY }))
+      )
+      .subscribe(value => console.log(value));
   }
 
   filterOperator() {
@@ -38,15 +38,39 @@ export class OperatorsComponent implements OnInit {
     const observableInterval = interval(1000);
 
     observableFrom
-    .pipe(
-      filter(value => value.includes('()'))
-    )
-    .subscribe(value => console.log(value));
+      .pipe(
+        filter(value => value.includes('()'))
+      )
+      .subscribe(value => console.log(value));
 
     observableInterval
-    .pipe(
-      filter(value => value % 2 === 0)
-    )
-    .subscribe(value => console.log(value));
+      .pipe(
+        filter(value => value % 2 === 0),
+        map(interval => console.log('Interval:', interval)),
+        delay(1000)
+      )
+      .subscribe(value => console.log(value));
+  }
+
+  tapOperator() {
+    const observableInterval = interval(1000);
+
+    observableInterval
+      .pipe(
+        tap(interval => console.warn(`[Tap] Interval - ${interval}`)),
+
+        tap(interval => console.log('[Tap] Initial:', interval)),
+        filter(interval => interval % 2 === 0),
+
+        tap(interval => console.log('[Tap] Filters:', interval)),
+        map(interval => `Interval: ${interval}`),
+
+        tap(interval => console.log('[Tap] Mapping:', interval)),
+        delay(1000)
+      )
+      .subscribe(interval => {
+        console.log('[Tap] Result:', interval);
+        console.log('---');
+      });
   }
 }
